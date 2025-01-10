ef1c7c62c11cb7d2bca64653b9b1667ba75589a2
Seb Mascha <sebastien@summerrobotics.ai>
2025-01-10 13:02:15 +0100

    :bricks: Improve the docker build and run pipeline

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,21 @@
-docker pull colmap/colmap:latest
-docker run --gpus all -w /working -v $1:/working -it colmap/colmap:latest
+#!/bin/bash
+
+set -ex
+
+docker build -t="colmap:latest" --build-arg CUDA_ARCHITECTURES=75 .
+
+docker run \
+    -it \
+    --runtime nvidia --gpus all -e NVIDIA_DRIVER_CAPABILITIES=all \
+    --env="DISPLAY" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env HOME="$HOME" \
+    --workdir="$PWD" \
+    --volume "$HOME":"$HOME" \
+    --env HISTFILE="$HOME"/.bash_history_colmap \
+    colmap:latest
+
+# Might required: 'xhost + local:' on the host machine
+
+# docker pull colmap/colmap:latest
+# docker run --gpus all -w /working -v $1:/working -it colmap/colmap:latest
